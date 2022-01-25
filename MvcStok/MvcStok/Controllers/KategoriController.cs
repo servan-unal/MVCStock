fb569076d66e22a97bdd9f0d781be341d8f2abfa
servan-unal <71839049+servan-unal@users.noreply.github.com>
2022-01-25 19:37:55 +0300

    stock project MVC

---
+++ MvcStok/MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity; // Model içindeki entity klasörüne ulaşıyorum.
+using PagedList;
+using PagedList.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        // tablolarımı tutan modelim yani mvcdbstokentities sınıfından nesne üretiyorumki tablolarıma ulaşabileyim.
+        public ActionResult Index(int sayfa=1)
+        {
+            var degerler = db.TBLKATEGORILER.ToList().ToPagedList(sayfa, 4);
+            //var degerler = db.TBLKATEGORILER.ToList();
+            // degerler değişkeniyle db nesnemdeki tablomu listeliyorum. ve degerleri gösteriyorum.
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult Yenikategori()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniKategori");
+            }
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult SIL(int id)
+        {
+            var kategori = db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir (int id)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir", ktgr);
+        }
+
+        public ActionResult Guncelle (TBLKATEGORILER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD = p1.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+            // p1 parametresi yarattım ve sonrasında ktg nesnesi oluşturup tablom içinden p1.kategoriid mi bul dedim.
+            //parametredeki adımı nesnemedekine attım
+        }
+    }
+}
---
+++ MvcStok/MvcStok/Controllers/UrunController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            List<SelectListItem> 
+                degerler = (from i in db.TBLKATEGORILER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+                                             }
+                 ).ToList();
+            ViewBag.dgr = degerler;
+            //Bu işlemlerin hepsi get işlemi yapıldığında yani sayfa yüklendiğinde yapılacak [httpget]
+            //Yukarıdaki kod bloğunda linq sorgusu yaptım. önce tblkategolerin listesini çek sonra bunu i değişenine ata.
+            // bu yeni listeyi seçiyorum ve sonrasında textini ve ıd sini çekeceğim için...
+            // text e listemdeki textleri, value ile id lerini alıyorum ve sonrasında listeliyorum.
+            //viewbag.dgr ile de  controller içindeki ifadeyi başka yere taşıyacağım. dgr nesnesi oluşturup başka yerde kullacanağım.
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniUrun(TBLURUNLER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+            p1.TBLKATEGORILER = ktg;
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            //Kategorideki ıd yi almak için linq sorgusu kullandım. bloğun ilk kodunda where p1 den gelen
+            // kategorilerdeki katıgoriıd yi getirecek.firstofdefault linq da (seçilen ilk değeri getirir.)
+            // ktg yi p1 kategorilere ata.
+            //sonra redirecttoaction da kaydet yapıldıktan sonra istediğim sayfaya götürüyor.
+        }
+
+        public ActionResult SIL (int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            db.TBLURUNLER.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            //Bu blokta db nesnesinin ürünler sayfasındaki id lerini bul.
+            //sonrasında kategoriden gelen değeri sil deyip kaydedip ındex sayfasına beni yönlendir dedim.
+        }
+
+        public ActionResult UrunGetir (int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            List<SelectListItem>
+                degerler = (from i in db.TBLKATEGORILER.ToList()
+                            select new SelectListItem
+                            {
+                                Text = i.KATEGORIAD,
+                                Value = i.KATEGORIID.ToString()
+                            }
+                 ).ToList();
+            ViewBag.dgr = degerler;
+            return View("UrunGetir", urun);
+        }
+        public ActionResult Guncelle (TBLURUNLER p)
+        {
+                var urun = db.TBLURUNLER.Find(p.URUNID);
+                urun.URUNAD = p.URUNAD;
+                urun.MARKA = p.MARKA;
+                urun.STOK = p.STOK;
+                urun.FIYAT = p.FIYAT;
+                //urun.URUNKATEGORI = p.URUNKATEGORI;
+                var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+                urun.URUNKATEGORI = ktg.KATEGORIID;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            
+            
+        }
+    }
+}
